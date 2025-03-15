6fb48565c83299da338378faee6e300adfd80cfb
A5hish-star <ashishkumbhar6533@gmail.com>
2025-03-15 19:08:24 +0530

    Initial commit - C# console app with GitHub Action

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyconsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world from my console app!");
+
+            // If the "--no-wait" argument is not provided, wait for user input.
+            if (!Array.Exists(args, arg => arg == "--no-wait"))
+            {
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
